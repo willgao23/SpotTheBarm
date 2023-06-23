401c79af2675bf22ea289e6a7916a5b0a60a6e2b
wgao23 <wgao23@students.cs.ubc.ca>
2023-06-22 23:10:17 -0700

    Add in random movement around the canvas

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
+var timeleft = 1000;
+var notfound = true;
 function start_countdown() {
-    var timeleft = 1000;
     setInterval(function () {
         var countdown = document.getElementById("countdown");
         if (timeleft < 0) {
@@ -17,8 +18,64 @@ function start_countdown() {
         timeleft -= 1;
     }, 10);
 }
+function generate_new_pos(container) {
+    if (container) {
+        var h = container.clientHeight;
+        var w = container.clientWidth;
+        var new_h = Math.floor(Math.random() * h);
+        var new_w = Math.floor(Math.random() * w);
+        return [new_h, new_w];
+    }
+    else {
+        return null;
+    }
+}
+function animate_elem(elem) {
+    var container = document.getElementById("container");
+    var arr = generate_new_pos(container);
+    var elem_pos = elem.getBoundingClientRect();
+    var top = elem_pos.top;
+    var left = elem_pos.left;
+    var animate = setInterval(function () {
+        if (arr) {
+            if (top == arr[0] && left == arr[1]) {
+                if (notfound) {
+                    arr = generate_new_pos(container);
+                }
+                else {
+                    clearInterval(animate);
+                }
+            }
+            else if (top < arr[0] && left < arr[1]) {
+                top += 1;
+                left += 1;
+            }
+            else if (top > arr[0] && left > arr[1]) {
+                top -= 1;
+                left -= 1;
+            }
+            else if (top < arr[0]) {
+                top += 1;
+            }
+            else if (top > arr[0]) {
+                top -= 1;
+            }
+            else if (left < arr[1]) {
+                left += 1;
+            }
+            else {
+                left -= 1;
+            }
+            elem.style.top = top + 'px';
+            elem.style.left = left + 'px';
+        }
+    }, 1);
+}
 function start_game() {
     var barm = document.getElementById("barm");
+    if (barm) {
+        animate_elem(barm);
+    }
     start_countdown();
 }
 document.addEventListener("DOMContentLoaded", function () {
--- game.ts
+++ game.ts
@@ -1,5 +1,7 @@
+let timeleft: number = 1000;
+let notfound: boolean = true;
+
 function start_countdown() {
-    let timeleft: number = 1000;
     setInterval(function() {
         let countdown = document.getElementById("countdown");
         if (timeleft < 0) {
@@ -16,8 +18,58 @@ function start_countdown() {
     }, 10);
 }
 
+function generate_new_pos(container: HTMLElement | null): any[] | null {
+    if (container) {
+        let h: number = container.clientHeight;
+        let w: number = container.clientWidth;
+        let new_h = Math.floor(Math.random() * h);
+        let new_w = Math.floor(Math.random() * w);
+        return [new_h, new_w];
+    } else {
+        return null;
+    } 
+}
+
+function animate_elem(elem: HTMLElement) {
+    let container = document.getElementById("container");
+    let arr: any[] | null = generate_new_pos(container);
+    let elem_pos = elem.getBoundingClientRect();
+    let top: number = elem_pos.top;
+    let left: number = elem_pos.left;
+    let animate = setInterval(function() {
+        if (arr) {
+            if (top == arr[0] && left == arr[1]) {
+                if (notfound) {
+                    arr = generate_new_pos(container);
+                } else {
+                    clearInterval(animate);
+                }
+            } else if (top < arr[0] && left < arr[1]) {
+                top += 1;
+                left += 1;
+            } else if (top > arr[0] && left > arr[1]) {
+                top -= 1;
+                left -= 1;
+            } else if (top < arr[0]) {
+                top += 1;
+            } else if (top > arr[0]) {
+                top -= 1;
+            } else if (left < arr[1]) {
+                left +=1;
+            } else {
+                left -= 1;
+            }
+            elem.style.top = top + 'px';
+            elem.style.left = left + 'px';
+        }     
+    }, 1);    
+}
+
 function start_game() {
-    let barm: object | null = document.getElementById("barm");
+    let barm: HTMLElement | null = document.getElementById("barm");    
+    if (barm) {
+        animate_elem(barm);
+    }
     start_countdown();
 }
 
